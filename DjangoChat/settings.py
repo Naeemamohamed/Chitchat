b4b78e72f1ec41072467543d26f46b8833bbb2d3
Naeemamohamed <mohamednaima2006@gmail.com>
2024-07-20 09:58:58 +0300

    change in installed apps

--- DjangoChat/settings.py
+++ DjangoChat/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS =['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'ChitChat.apps.ChitchatConfig',
+    'ChitChat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
